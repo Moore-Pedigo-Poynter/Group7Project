57e4cfc0b43b6255454ae9ef466a7110a745a5b0
XJM <xander_moore1@mymail.eku.edu>
2026-04-21 15:24:59 -0400

    Minor edits and commented up

--- Group7Project/PassViewController.swift
+++ Group7Project/PassViewController.swift
@@ -22,19 +22,24 @@ class PassViewController: UIViewController {
     var password = ""
     
     @IBAction func editedPassField(_ sender: Any) {
-        savePass.isEnabled = !passField.text!.isEmpty
+        // Because we allow the user to edit their password directly, we have
+        // to revalidate it each time it's changed. We hide the save button if
+        // their password is invalid
+        savePass.isEnabled = (!passField.text!.isEmpty && (passField.text!.count >= 8))
     }
     
     
     @IBAction func generateButton(_ sender: Any) {
+        // These strings let us generate random valid characters. The symbols
+        // may need to be altered if some aren't typically allowed in passwords
         let caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         let lowers = "abcdefghijklmnopqrstuvwxyz"
         let nums = "0123456789"
         let symbols = "!@#$%^&*()/,.<>;:'\"{}-+"
         var allValidChars = ""
-        password = ""
         var passArray = [Character]()
         
+        // Ensures we have a valid input for length
         guard let length = Int(passwordLengthField.text ?? "") else {
             return
         }
@@ -48,6 +53,7 @@ class PassViewController: UIViewController {
             allValidChars += caps
         }
         
+        // We do the above code for every valid type
         if lowersEnabled.isOn {
             let char = lowers.randomElement() ?? "a"
             passArray.append(char)
@@ -66,8 +72,11 @@ class PassViewController: UIViewController {
             allValidChars += symbols
         }
         
+        // If the length is valid, we continue generating until the password
+        // is full-length
         if length < 8 {
-            passwordLengthField.text = "Too short!";
+            passwordLengthField.text = "Too short!"
+            return
         }
         else {
             for _ in 0..<(length - passArray.count) {
@@ -81,6 +90,8 @@ class PassViewController: UIViewController {
         
         password = String(passArray)
         
+        // We use either asterisks or the characters depending on if we're wanting
+        // it to be shown
         if showPass.isOn {
             passField.text = password
         }
@@ -88,10 +99,13 @@ class PassViewController: UIViewController {
             passField.text = String(repeating: "*", count: password.count)
         }
         
+        // Because our password is already validated, we can always enable the
+        // save button when it's generated
         savePass.isEnabled = true
     }
     
     @IBAction func shownChanged(_ sender: Any) {
+        // Updates the password's readability
         if showPass.isOn {
             passField.text = password
         }
@@ -101,12 +115,11 @@ class PassViewController: UIViewController {
     }
     
     @IBAction func savePassButton(_ sender: Any) {
+        // Needs to send the password property to the list in the other VC
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
 }
